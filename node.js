8cbddd2018209a6e8768f0203192edcbe4c8d58a
legendss <david.l.castilla@gmail.com>
2015-06-05 17:03:05 -0400

    testing request with timeout

--- node.js
+++ node.js
@@ -3,6 +3,10 @@ var http =  require('http');
 http.createServer(function(request, response){
   response.writeHead(200);
   response.write("Test server");
-  response.end();
+  setTimeout(function(){
+    response.write("with timeout")
+    response.end();
+  }, 5000);
+
 }).listen(8080);
 console.log('listening on port 8080...')
